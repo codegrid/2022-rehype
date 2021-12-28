e88dd8640e13fe016b7acc95c256532adfa49f3a
Takeshi Takatsudo <takazudo@gmail.com>
2021-12-28 15:18:49 +0900

    added comments

--- 01-tweak-html/index.js
+++ 01-tweak-html/index.js
@@ -15,24 +15,43 @@ const html = `
 </ul>
 `;
 
-const processor = unified()
-  .use(rehypeParse, { fragment: true })
-  .use(rehypeWrapAll, [
-    {
-      selector: "h1",
-      wrapper: "div.h1-wrapper",
-    },
-    {
-      selector: "p",
-      wrapper: "div.p-wrapper",
-    },
-  ])
-  .use(rehypeAddClasses, {
-    ul: "text-list",
-    li: "text-list-item",
-  })
-  .use(rehypeFormat)
-  .use(rehypeStringify);
+// まずはProcessorを作成
+const processor = unified();
+
+// rehype-parseプラグインを使う
+// - HTML文字列を構文木に変換するParser
+// - HTML文字列の一部を使うオプションを指定
+processor.use(rehypeParse, { fragment: true });
+
+// rehype-wrap-allプラグインを使う
+// - <h1>を<div class="h1-wrapper">で包む
+// - <p>を<div class="p-wrapper">で包む
+processor.use(rehypeWrapAll, [
+  {
+    selector: "h1",
+    wrapper: "div.h1-wrapper",
+  },
+  {
+    selector: "p",
+    wrapper: "div.p-wrapper",
+  },
+]);
+
+// rehype-add-classesプラグインを使う
+// - <ul>に"text-list"クラスを追加
+// - <li>に"text-list-item"クラスを追加
+processor.use(rehypeAddClasses, {
+  ul: "text-list",
+  li: "text-list-item",
+});
+
+// rehype-formatプラグインを使う
+// いい感じにインデントなど揃えてくれる
+processor.use(rehypeFormat);
+
+// rehype-stringifyプラグインを使う
+// - 構文木をHTML文字列に変換するCompiler
+processor.use(rehypeStringify);
 
 // HTML文字列を処理してVFileを作成
 const vfile = processor.processSync(html);
